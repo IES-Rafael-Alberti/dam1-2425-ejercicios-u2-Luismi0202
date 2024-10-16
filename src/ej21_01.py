63c039d907fd0c53981158edf34e5098ff839bd6
Luismi0202 <gdluis@iesrafaelalberti.es>
2024-10-16 19:43:10 +0200

    Ejercicio  1 casi hecho

--- src/ej21_01.py
+++ src/ej21_01.py
@@ -1,39 +1,34 @@
 #Ejercicio 2.1.1
 #Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
-mensaje_de_error= "ERROR, INTRODUCE TU EDAD DE VERDAD"
 
 def introducir_edad(valor):
-    valor= input()
+    valor = input()
     return valor
-def valor_es_num(valor:str):
-    valor= valor.strip
-    contarpuntos= valor.count(".")
-    contarguion = valor.count("-")
-    if contarpuntos >= 1:
+
+def comprobar_si_num(valor:str):
+    valor.strip
+    if valor.count(".")>=1:
         return False 
-    elif contarguion >1:
+    elif valor.count("-")>=1:
         return False
-    elif valor.startswith("-"):
-        valor = valor.replace("-","")
-    return valor.isdigit
+    elif valor.find("-"):
+        valor = valor[0:]
+        return valor.isdigit()
 
-def mayor_edad(num):
-    if num >=18:
-        return "¡¡EHNORABUENA, ERES MAYOR DE EDAD!! ESTÁS INVITADO A UNA CERVECITA!!"
-    elif num <18:
-        return "Lo siento, no estás listo para la vida de adulto... eres menor...."
-    
-def msj_error(mensaje_de_error):
-    return mensaje_de_error
+def mayor_de_edad(num):
+    if num <18:
+        return"lo siento eres menor adiós *se va*"
+    elif num >=18:
+        return "¡¡PERFECTO, ERES MAYOR DE EDAD!!¡¡VAMONOS A POR CERVEZA!!"
+
 def main():
-    valor= str
-    print("Introduce tu edad:")
-    num= introducir_edad(valor)
-    valor_es_num(valor)
-    while valor_es_num(valor)!=True:
-        msj_error()
-        valor=input()
-    print(mayor_edad(num))
-
+    valor = 0
+    num = 0
+    print("¡Te invito a una cer-! Espera, ¿CUÁNTOS AÑOS TIENES?")
+    num = introducir_edad(valor)
+    num = comprobar_si_num(num)
+    if num == True:
+        print(mayor_de_edad(num))
+        
 if __name__ == "__main__":
     main()
