5e656fd95f17b835834c73029be86052ddeeffa1
Luis Miguel Gomila Dominguez <gdluis@iesrafaaelalberti.es>
2024-10-23 00:10:36 +0200

    Ejercicios del 1 al 10 hechos

---
+++ src/ej21_09.py
@@ -0,0 +1,79 @@
+#Ejercicio 2.1.9
+#Escribir un programa para una empresa que tiene salas de juegos para todas las edades y quiere calcular de forma automática el precio que debe cobrar a sus clientes por entrar. El programa debe preguntar al usuario la edad del cliente y mostrar el precio de la entrada. Si el cliente es menor de 4 años puede entrar gratis, si tiene entre 4 y 18 años debe pagar 5€ y si es mayor de 18 años, 10€.
+
+
+"""
+Args:
+edad = input() -> Usuario escribe edad aquí
+
+Returns:
+retorna la edad que haya puesto
+"""
+def introduce_edad():
+    edad = input()
+    return edad
+
+"""
+Args:
+valor = valor.strip()-> Elimina los huecos
+valor.startswith("-")-> Marcará si el valor empieza con guión. Como no queremos negativos retornaremos False luego.
+valor.count(".")-> Se contara si hay porque no queremos ni decimales ni caracteres
+valor.isdigit()-> comprobará si valor es un número
+
+Returns:
+Devolvera siempre booleanos (True o False)
+"""
+def comprobar_edad(valor:str):
+    valor= valor.strip()
+    if valor.startswith("-"):
+        return False
+    elif valor.count(".") >= 1:
+        return False
+    if valor.isdigit() == False:
+        return False
+    else: #si no es número se mira si es negativo (para poder descartarlos)
+        valor = int(valor)
+        if valor <0:
+            return False
+        else:
+            return True
+"""
+Args:
+If valor <4, ==4 and <=18, >18 -> Rango de valores que puede tomar cada if
+
+Returns:
+En cada uno retornará una cadena de caracteres diciendo cuanto tiene que pagar
+"""
+def precio_entrada(valor):
+    if valor <4:
+        return "¡Ehnorabuena chaval! Entras totalmente GRATIS"
+    if valor ==4 and valor <=18:
+        return "Pues ya sabes... para entrar tendrás que darme 5€"
+    if valor >18:
+        return "Ya puedes ir dándome 10€ si deseas entrar..."
+
+"""
+Función que pide edad, llama a la función introducir edad.
+Luego, se comprueba que esa edad sea correcta, si no da error.
+la edad se transforma en entera y pasa a mirarse cual es el precio
+de su entrada con una llamada a la función correspondiente.
+"""
+def main():
+    print("Si quieres pasar, primero tendrás que darme tu edad...")
+    valor = introduce_edad()
+    while comprobar_edad(valor) == False:
+        print("*ERROR* Eso no es una edad... Dimela de nuevo:")
+        valor = introduce_edad()
+    edad = int(valor)
+    print (precio_entrada(edad))
+
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
--- src/ej21_10.py
+++ src/ej21_10.py
@@ -4,12 +4,28 @@
 Ingredientes no vegetarianos: Peperoni, Jamón y Salmón."""
 #Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en función de su respuesta le muestre un menú con los ingredientes disponibles para que elija. Solo se puede eligir un ingrediente además de la mozzarella y el tomate que están en todas la pizzas. Al final se debe mostrar por pantalla si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.
 
+"""
+Args:
+tipo = input() -> Poner el tipo de pizza o ingrediente
 
-
-def introduce_pizza():
+Returns: 
+tipo = el tipo de pizza o ingrediente introducido
+"""
+def introduce_valor():
     tipo = input()
     return tipo 
 
+
+"""
+Args:
+if tipo.lower() == vegetariana -> Si es vegetariana...
+if tipo.lower() == no vegetariana -> Si no es vegetariana...
+
+Returns:
+pimiento y tofu si es vegetariana, 
+peperoni,jamon y salmon si es no vegetariana
+Error -> Si no es ninguna de las dos devolvera un valor llamado error
+"""
 def ingredientes(tipo:str):
     if tipo.lower() == "vegetariana":
         return "Pimiento, tofu"
@@ -17,24 +33,50 @@ def ingredientes(tipo:str):
         return "Peperoni, Jamón y Salmón"
     else:
         tipo = "Error"
+        return tipo
 
-def menu():
-    
+"""
+Args:
+if tipo.lower()== vegetariana -> Si es vegetariana...
+    if ingrediente.lower() == "pimiento" or "tofu" -> si es uno de estos ingredientes...
+if tipo.lower()== no vegetariana -> Si es no vegetariana...
+    if ingrediente.lower() == "peperoni" or "jamón" or "salmón"-> si es uno de estos ingredientes...
+
+Returns:
+Puede retornar o ingrediente o falso dependiendo de si el valor es uno de los que se te piden o no
+"""
+def selección_ingredientes(tipo:str,ingrediente:str):
+    if tipo.lower() == "vegetariana":
+        if ingrediente.lower()== "pimiento" or ingrediente.lower()=="tofu":
+            return ingrediente
+        else:
+            return "Error"
+    elif tipo.lower() == "no vegetariana":
+        if ingrediente.lower() == "peperoni" or ingrediente.lower() == "jamón" or ingrediente.lower()== "salmón":
+            return ingrediente
+        else:
+            return "Error"
 
 
-
-
-
+"""
+Función principal. Pide que pizza quieres, la introduces a través de una función.
+Si le has introducido una de las dos que te dicen seguirá comprobando otras cosas, sino,
+te dara un error. Luego te pedirá ingrediente y pasará lo mismo, si le das uno de los que te da,
+no pasará nada, de lo contrario, dará error.
+La función acabara con un print a el tipo de pizza, al ingrediente elegido y una cadena de caracteres.
+"""
 def main():
     print("ESCRIBA: ¿Que pizza quieres? \n A)Vegetariana (Escriba vegetariana) \n B)No vegetariana (Escriba no vegetariana)")
-    tipo = introduce_pizza()
+    tipo = introduce_valor()
     while ingredientes(tipo) == "Error":
         print("**ERROR** ¡¡¡¡Tienes que introducir (Vegana) o (No vegana) en función del menú que quieras!!!!")
-        tipo = introduce_pizza()
-    print(f"Elija ingredientes {ingredientes(tipo)}")
-
-
-
+        tipo = introduce_valor()
+    print(f"Elija un ingrediente entre estos: {ingredientes(tipo)}")
+    ingrediente= introduce_valor()
+    while selección_ingredientes(tipo,ingrediente)== "Error":
+        print(f"**ERROR** Introduce un tipo de ingrediente válido para tu pizza {tipo}")
+        ingrediente = introduce_valor()
+    print(f"Entonces quieres una pizza {tipo} con los siguientes ingredientes: Mozarella, tomate,{selección_ingredientes(tipo,ingrediente)}... ¡Oido cocina!")
 
 
 
