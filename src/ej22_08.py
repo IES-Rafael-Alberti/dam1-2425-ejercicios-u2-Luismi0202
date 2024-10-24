9dc0cccac56a5e7aa7363e4c0157d8257e3b7522
Luis Miguel Gomila Dominguez <gdluis@iesrafaaelalberti.es>
2024-10-24 11:43:05 +0200

    Ejercicio 10 y ejercicio 8 modificado

--- src/ej22_08.py
+++ src/ej22_08.py
@@ -1,5 +1,11 @@
+"""
+Args:
+num = input()-> Input para que el usuario meta un número
+Prueba si ese número es entero
 
-
+Returns: 
+Devuelve el número o que es falso dependiendo de si es número o no.
+"""
 def introduce_num():
     num = input()
     try:
@@ -8,13 +14,30 @@ def introduce_num():
     except ValueError:
         return False
 
+"""
+Args:
+for i in range(1,num+1): --> Bucle que hará las vueltas hasta el valor que le hayamos introducido
+if i%2==1: -->Mientras que el valor de la vueltaeste en número impar, se hará posible el print
+triangulo = "" --> Reseteo de triangulo para que vaya haciendo prints por columnas
+for x in range (i,0,-2): --> Se ponen los númeroos del reves
+triangulo+= f" {x} "
+print(triangulo) --> Imprime por pantalla el resultado del triángulo
+
+Returns:
+"""
 def triangulo_impar(num:int):
-    triangulo = ""
+    
     for i in range(1,num+1):
         if i%2==1:
-            triangulo+= f" {i} "
+            triangulo = ""
+            for x in range (i,0,-2):
+                triangulo+= f" {x} "
             print(triangulo)
-
+"""
+Función principal que pide un número entero. Si el número no es entero dará fallo.
+De lo contrario, el valor introducido se pasará a número entero y luego, pedirá a la función triangulo_impar que le haga un triangulo hasta 
+el valor impar del número introducido.
+"""
 def main():
     print("Dame un número entero")
     num = introduce_num()
---
+++ src/ej22_10.py
@@ -0,0 +1,81 @@
+#Ejercicio 2.2.10¶
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no.
+
+
+"""
+Args:
+num = inpuut()-> Pide al usuario que ingrese un número
+
+Returns:
+Retorna el número ingresado por el usuario.
+"""
+def introduce_num():
+    num = input()
+    return num
+
+
+"""
+Args:
+Intenta pasar valor a entero:
+Si lo consigue, devuelve el valor.
+Si no lo consigue, Devuelve falso.
+
+Returns:
+Devuelve falso o el valor dependiendo de si es un número correcto o no.
+"""
+def comprobar_num(valor):
+    try:
+        valor =valor(int)
+        return valor
+    except ValueError:
+        return False
+
+
+"""
+Args:
+UN NÚMERO PRIMO ES AQUEL QUE ES SOLAMENTE DIVISIBLE ENTRE EL MISMO Y 1
+Sabiendo esto, creamos una variable contador y un bucle for desde 1 hasta el número.
+Cada vez que el número sea divisible por un número, se almacenará un +1 en el contador.
+Si el contador es igual a dos, quiere decir que ese número solamente es divisible por dos valores (el mismo y 1)
+De lo contrario, no será primo.
+
+Returns:
+Devuelve si el número es primo o no en función de la variable contador.
+"""
+def es_primo(num):
+    contador = 0
+    for i in range(1,num+1):
+        if num%i==0:
+            contador =contador+1
+    if contador ==2:
+        return "Su número es primo"
+    else:
+        return "Su número NO es primo"
+
+
+"""
+Función principal que pide un número.
+Comprueba si este número introducido es válido, si no lo es da error.
+Si el número introducido es válido, se pasara el valor a entero y se mirará si es primo.
+Se imprime por pantalla el valor que retorna la función "es_primo"
+"""
+def main():
+    print("Dame un número entero")
+    valor = introduce_num()
+    while valor == False:
+        print("**ERROR** Valor inválido. Introduce un número entero:")
+        valor= introduce_num()
+    num = int(valor)
+    print(es_primo(num))
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
